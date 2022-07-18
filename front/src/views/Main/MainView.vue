05140060f7a53487cfbc1e85e56002aff1d1f95b
ParkSeongHyeon <24ph00@naver.com>
2022-07-19 03:08:13 +0900

    feature - ui

--- front/src/views/Main/MainView.vue
+++ front/src/views/Main/MainView.vue
@@ -1,3 +1,40 @@
 <template>
-  <div class="main">메인화면 입니다</div>
+  <div>
+    <MainBar @toggle="toggleOnOff"/>
+    <section>
+    <SideBar v-if="isOn"/>
+    <HomeLogout/>
+    </section>
+  </div>
 </template>
+
+
+<script>
+  import MainBar from "./components/mainbar.vue"
+  import SideBar from "./components/sidebar_logout.vue"
+  import HomeLogout from "./components/home_logout.vue"
+  export default {
+    name: "MainView",
+    components: {
+      MainBar,
+      SideBar,
+      HomeLogout,
+    },
+    data() {
+      return {
+        isOn : false
+      }
+    },
+    methods: {
+      toggleOnOff: function() {
+        this.isOn = !this.isOn
+      }
+    }
+  }
+</script>
+
+<style>
+  section {
+    display: flex;
+  }
+</style>
---
+++ front/src/views/Main/components/sidebar_logout.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="sidebar">
+        <a href="#" >로그인</a>
+        <span>이 필요합니다.</span>
+        <hr>
+
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+    .sidebar {
+        background-color:rgb(242,242,242);
+        align-items: flex-start;
+        width: 20%;
+        height:100vh;
+        padding: 1rem;
+        margin:0;
+        position: absolute;
+    }
+    a {
+        text-decoration: none;
+        color: gray;
+        font-weight:bold;
+    }
+    
+</style>
