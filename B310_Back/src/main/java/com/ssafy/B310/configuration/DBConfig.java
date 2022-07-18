46dfe2030e082dbd6efbf2087c32fd5e17398a36
SSAFY <ho6778@naver.com>
2022-07-18 16:07:28 +0900

    user crud by JPA

--- B310_Back/src/main/java/com/ssafy/B310/configuration/DBConfig.java
+++ B310_Back/src/main/java/com/ssafy/B310/configuration/DBConfig.java
@@ -1,8 +1,8 @@
-package com.ssafy.B310.configuration;
-
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@MapperScan(basePackages = "com.ssafy.B310.mapper")
-public class DBConfig {}
+//package com.ssafy.B310.configuration;
+//
+//import org.mybatis.spring.annotation.MapperScan;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//@MapperScan(basePackages = "com.ssafy.B310.mapper")
+//public class DBConfig {}
