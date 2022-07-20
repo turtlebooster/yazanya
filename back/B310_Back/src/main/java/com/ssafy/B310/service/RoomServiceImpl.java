60a6edd74bdba07e6ed312117f543b65cb82d4b8
turtlebooster <jhchoi6778@gmail.com>
2022-07-20 16:17:49 +0900

    DB Room service and controller update

--- back/B310_Back/src/main/java/com/ssafy/B310/service/RoomServiceImpl.java
+++ back/B310_Back/src/main/java/com/ssafy/B310/service/RoomServiceImpl.java
@@ -1,12 +1,14 @@
 package com.ssafy.B310.service;
 
 import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ssafy.B310.entity.Room;
-import com.ssafy.B310.entity.User;
 import com.ssafy.B310.repository.RoomRepository;
 
 @Service
@@ -16,15 +18,86 @@ public class RoomServiceImpl implements RoomService {
 	RoomRepository roomRepository;
 	
 	@Override
-	public int registRoom(Room room) throws SQLException {
+	public int createRoom(Room room) throws SQLException {
+		// JPA save로 반환되는 객체가 이번에 생성된 room 인지?
+		Room newRoom = roomRepository.save(room);
+		return newRoom.getRoomNum();
+	}
+
+
+
+	@Override
+	public int updateRoom(Room room) throws SQLException {
+		Optional<Room> oRoom = roomRepository.findById(room.getRoomNum()); 
+		// 해당 방이 있을경우
+		if (oRoom.isPresent()) {
+			Room r = oRoom.get();
+			// 해시태그는 여기서 업데이트 못함
+			// 방 제목과 알람설정 수정
+			r.setRoomName(room.getRoomName());
+			r.setRoomStudyTime(room.getRoomStudyTime());
+			r.setRoomRestTime(room.getRoomRestTime());
+			roomRepository.save(r);
+			return 1;
+		}
+		// 없을경우
+		return 0;
+	}
+
+
+
+	@Override
+	public List<Room> filterRoom(Map<String, Integer> params) throws SQLException {
+		int video = params.get("video");
+		int sound = params.get("sound");
+		int fullcheck = params.get("fullcheck");
 		
-		// 이미 해당 아이디나 이메일로 가입된 유저가 있을경우
-		if (roomRepository.findByRoomName(room.getRoomName()).isPresent()) {
-			return 0;
+		switch (video) {
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
 		}
 		
-		// 없을경우
-		roomRepository.save(room);
-		return 1;
+		switch (sound) {
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		}
+		
+		switch (fullcheck) {
+		case 1:
+			break;
+		case 2:
+			break;
+		}
+		
+		return roomRepository.findAll();
 	}
+
+
+
+	@Override
+	public int removeRoom(int roomNum) throws SQLException {
+		Optional<Room> oRoom = roomRepository.findById(roomNum);
+		// 해당 방이 있을경우
+		if (oRoom.isPresent()) {
+			Room r = oRoom.get();
+			roomRepository.deleteById(roomNum);
+			return 1;
+		}
+		// 없을경우
+		return 0;
+	}
+
+
+
+	
+
+
 }
