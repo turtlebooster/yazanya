17a26ae216f6f212c4cbb8526352d2ed08058cca
jihoyangKR <qqq5901@naver.com>
2022-07-20 17:40:00 +0900

    RoomController.removeRoom edit

--- back/B310_Back/src/main/java/com/ssafy/B310/controller/RoomController.java
+++ back/B310_Back/src/main/java/com/ssafy/B310/controller/RoomController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -46,8 +47,8 @@ public class RoomController {
 	}
 
 	// 방 삭제
-	@GetMapping("/remove")
-	public ResponseEntity<?> removeRoom(@RequestBody int roomNum) throws SQLException{
+	@GetMapping("/remove/{roomNum}")
+	public ResponseEntity<?> removeRoom(@PathVariable("roomNum") int roomNum) throws SQLException{
 		
 		int cnt = roomservice.removeRoom(roomNum);
 		
