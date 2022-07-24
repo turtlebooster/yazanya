798b56f45cf3e2a3a4a94c2b8070a0af43784afc
JunHyuk Park <73526186+inte9ral@users.noreply.github.com>
2022-07-25 07:09:21 +0900

    초록색 좀 더 진하게 색감 테스트 중

--- front/src/views/Main/components/MainMypage.vue
+++ front/src/views/Main/components/MainMypage.vue
@@ -1,9 +1,5 @@
 <template>
   <div class="main-mypage">
-    <div>
-      로비(마이페이지) 입니다. 본인 프로필 사진과 이제까지의 공부 플래너 전체 내역이 정리되어 나옵니다.
-    </div>
-    
     <div class="achievement">
       <div class="container">
         <div class="logo">
