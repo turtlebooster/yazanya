efa34f3ff9b0160950bb9490436a81946f512443
JunHyuk Park <73526186+inte9ral@users.noreply.github.com>
2022-07-25 09:03:37 +0900

    FE-feature-vscode 프리티어 설정 .prettierrc.json
    무슨 이유인지 적용 안되길래 최상단에 만들어놓음.

--- front/src/views/Main/MainView.vue
+++ front/src/views/Main/MainView.vue
@@ -1,47 +1,46 @@
 <template>
-	<div class="main-view">
-		<main-header-nav />
-		<div class="main-view-container">
-			<main-side-bar />
-			<div style="min-width: 104px;"></div>
-			<router-view class="router-view" />
-		</div>
-	</div>
+  <div class="main-view">
+    <main-header-nav />
+    <div class="main-view-container">
+      <main-side-bar />
+      <div style="min-width: 104px"></div>
+      <router-view class="router-view" />
+    </div>
+  </div>
 </template>
 
 <script>
-import MainHeaderNav from "@/views/Main/components/common/MainHeaderNav.vue";
-import MainSideBar from "@/views/Main/components/common/MainSideBar.vue";
+import MainHeaderNav from '@/views/Main/components/common/MainHeaderNav.vue';
+import MainSideBar from '@/views/Main/components/common/MainSideBar.vue';
 
 export default {
-	components: {
-		MainHeaderNav,
-		MainSideBar,
-	}
-
-}
+  components: {
+    MainHeaderNav,
+    MainSideBar,
+  },
+};
 </script>
 
 <style>
 .main-view {
-	width: 100vw;
-	height: 100vh;
+  width: 100vw;
+  height: 100vh;
 
-	display: flex;
-	flex-direction: column;
-	height: 100vh;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
 }
 .main-view-container {
-	flex-grow: 1;
-	display: flex;
+  flex-grow: 1;
+  display: flex;
 }
 .main-header-nav {
-	z-index: 25;
+  z-index: 25;
 }
 .main-side-bar {
-	z-index: 20;
+  z-index: 20;
 }
 .router-view {
-	flex-grow: 1;
+  flex-grow: 1;
 }
 </style>
