598604edf37f3a19d60f0ab6f5df6078acc2a8f7
최진호 <ho6778@naver.com>
2022-07-25 17:56:31 +0900

    trying to use querydsl

---
+++ back/B310_Back/src/main/java/com/ssafy/B310/repository/RoomQueryRepository.java
@@ -0,0 +1,15 @@
+package com.ssafy.B310.repository;
+
+import org.springframework.stereotype.Repository;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Repository
+public class RoomQueryRepository {
+	private final JPAQueryFactory queryFactory;
+	
+	public List<>
+}
--- back/B310_Back/src/main/java/com/ssafy/B310/service/RoomServiceImplAdd.java
+++ back/B310_Back/src/main/java/com/ssafy/B310/service/RoomServiceImplAdd.java
@@ -2,19 +2,16 @@ package com.ssafy.B310.service;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
-import com.ssafy.B310.entity.Hashtag;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.ssafy.B310.entity.Room;
-import com.ssafy.B310.entity.RoomHashtag;
 import com.ssafy.B310.repository.HashtagRepository;
 import com.ssafy.B310.repository.RoomHashtagRepository;
 import com.ssafy.B310.repository.RoomRepository;
@@ -30,7 +27,6 @@ public class RoomServiceImplAdd implements RoomService {
 	RoomHashtagRepository roomHashtagRepository;
 	@Autowired
 	HashtagRepository hashtagRepository;
-	
 	
 	@Override
 	public int createRoom(Room room) throws SQLException {
@@ -93,28 +89,7 @@ public class RoomServiceImplAdd implements RoomService {
 	// 해쉬태그 추천 목록
 	public List<Room> getRecommendHashtagList(List<Integer> hashtagNumList) {
 		
-		List<Room> list = roomHashtagRepository.findByHashtagHashtagNumIn(hashtagNumList);
-//		Set<Room> roomSet = new HashSet<Room>();
-//		roomSet.add(list.get(0).getRoom());
-//		System.out.println(roomSet.toString());
-		
-		/*
-		List<Hashtag> hashtagList = new ArrayList<Hashtag>();
-		for (int hashtagNum : hashtagNumList) {
-			hashtagList.add(hashtagRepository.findById(hashtagNum).get());
-//			System.out.println(hashtagNum);
-		}
-		
-//		System.out.println(hashtagList.get(0).getHashtagName());
-		
-//		System.out.println(hashtagList.toString());
-		List<RoomHashtag> roomNumList = roomHashtagRepository.findByHashtagIn(hashtagList);
-		List<Room> roomList = new ArrayList<Room>();
-		for (RoomHashtag rh : roomNumList) {
-//			System.out.println(rh.getId());
-			roomList.add(rh.getRoom());
-		}
-		*/
+
 		List<Room> roomList = new ArrayList<Room>();
 		return roomList;
 	}
