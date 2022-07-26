75faaf3f99e9c47748281f832fb0145ebf4e5e6c
YeoUl <41969902+YeoUlFox@users.noreply.github.com>
2022-07-26 17:29:24 +0900

    FE-feature - add hide main_nav function

--- front/src/views/common/components/MainNav.vue
+++ front/src/views/common/components/MainNav.vue
@@ -1,12 +1,9 @@
 <template>
-  <nav v-if="defalut_main_nav_flag" class="main-header-nav" :class="[ $root.theme ? 'light' : 'dark' ]">
+  <!-- Main Nav -->
+  <nav v-if="main_nav_on" class="main-header-nav" :class="[ $root.theme ? 'light' : 'dark' ]">
     <div class="h-100">
-      <router-link class="navbar-brand" to="/studyroom">
-        <!-- Main Nav -->
-        <img src="@/assets/logo/title_logo_009e73.png" class="d-inline-block align-top h-100 pl-3" alt="logo">
-
-        <!-- Study Room Nav -->
-        <img v-if="viewname === 'studyroom'" src="@/assets/logo/title_logo_009e73.png" class="d-inline-block align-center h-100" alt="logo">
+      <router-link class="navbar-brand" to="/">
+        <img src="@/assets/logo/title_logo_009e73.png" class="d-inline-block align-top h-100 ps-3" alt="logo">
       </router-link>
     </div>
   </nav>
@@ -23,29 +20,28 @@ export default {
   },
 
   setup(props) {
-    let defalut_main_nav_flag = ref(true);
-    let customed_nav_view_list = [
+    let hide_nav_view_list = [
+      // excluded paths
       'studyroom',
     ]
 
-    console.log(props.viewname);
-
-    customed_nav_view_list.forEach((viewname_item) => {
-      if(viewname_item == props.viewname) {
-        defalut_main_nav_flag.value = false;
-      }
-    })
-
-    console.log(defalut_main_nav_flag.value);
-
-    return { defalut_main_nav_flag }
+    var main_nav_on = ref(true);
+    hide_nav_view_list.forEach((item) => {
+        if(document.URL.includes(item) || props.viewname.includes(item)) {
+          return main_nav_on.value = false;
+        }
+      });
+    
+    return { main_nav_on }
   }
 }
 </script>
 
 <style scoped>
   .main-header-nav {
+    position: fixed;
     border-bottom: 4px solid #009e73;
-    height: 32px
+    height: 32px;
+    width: 100vw;
   }
 </style>
