9baf2980416012ed61cc85dba7b6ff71d79b7f5b
seoeun98 <vcv0174@naver.com>
2022-07-26 15:57:38 +0900

    room hashtag 완성 및 rest로 수정

--- back/B310_Back/src/main/java/com/ssafy/B310/repository/RoomHashtagRepository.java
+++ back/B310_Back/src/main/java/com/ssafy/B310/repository/RoomHashtagRepository.java
@@ -1,11 +1,20 @@
 package com.ssafy.B310.repository;
 
+import java.util.List;
+
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.ssafy.B310.entity.Hashtag;
+import com.ssafy.B310.entity.Room;
 import com.ssafy.B310.entity.RoomHashtag;
 
 @Repository
 public interface RoomHashtagRepository extends JpaRepository<RoomHashtag, Integer> {
-
+	List<RoomHashtag> findByRoom(Room room);
+	
+	@Transactional
+	int deleteByRoomAndHashtag(Room room, Hashtag hashtag);
 }
--- back/B310_Back/src/main/java/com/ssafy/B310/service/RoomHashtagService.java
+++ back/B310_Back/src/main/java/com/ssafy/B310/service/RoomHashtagService.java
@@ -4,14 +4,19 @@ package com.ssafy.B310.service;
 import java.sql.SQLException;
 import java.util.List;
 
+import com.ssafy.B310.entity.Hashtag;
+import com.ssafy.B310.entity.Room;
 import com.ssafy.B310.entity.RoomHashtag;
 
 public interface RoomHashtagService {
 	
 	//방 해쉬태그 목록
-	public List<RoomHashtag> getListRoomHashtag() throws SQLException;
+	public List<RoomHashtag> getRoomHashtagBy(Room room) throws SQLException;
 	
 	//방 해쉬태그 추가
 	public int addRoomHashtag(RoomHashtag rHt) throws SQLException;
+	
+	//방 해쉬태그 삭제
+	public int deleteRoomHashtag(Room room, Hashtag hashtag) throws SQLException;
 	
 }
