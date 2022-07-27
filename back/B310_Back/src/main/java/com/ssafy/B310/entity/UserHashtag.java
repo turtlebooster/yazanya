f0b2f0e36abc5ba4a97d7a6c89f6a5a0e2d5b946
seoeun98 <vcv0174@naver.com>
2022-07-27 13:40:52 +0900

    userHashtag 작성 완료

--- back/B310_Back/src/main/java/com/ssafy/B310/entity/UserHashtag.java
+++ back/B310_Back/src/main/java/com/ssafy/B310/entity/UserHashtag.java
@@ -7,7 +7,14 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 @Entity
+@Getter
+@Setter
+@ToString
 public class UserHashtag {
 	@Id
 	@GeneratedValue
@@ -24,5 +31,13 @@ public class UserHashtag {
 	@JoinColumn(name = "HASHTAG_NUM")
 	private Hashtag hashtag;
 	
+	public UserHashtag(User user, Hashtag hashtag) {
+		this.user = user;
+		this.hashtag = hashtag;
+	}
+	
+	public UserHashtag() {
+		
+	}
 
 }
