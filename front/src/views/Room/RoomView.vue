15c7e34c0467af5571d1e22477311600c2a1633f
wsu223 <wsu223@gmail.com>
2022-07-27 18:07:07 +0900

    FE-feature-add-roomnav
    set event and animation

--- front/src/views/Room/RoomView.vue
+++ front/src/views/Room/RoomView.vue
@@ -1,9 +1,31 @@
 <template>
-    <p> 스터디룸입니다. </p>
+  <div class="room-navbar" :class="[$root.theme ? 'light' : 'dark']">
+    <room-nav @toggle-planner="test()" @toggle-chat="test()"></room-nav>
+  </div>
 </template>
 
 <script>
-export default {
+import RoomNav from './components/RoomNavbar.vue';
 
+export default {
+  components: {
+    RoomNav,
+  },
+
+  setup() {
+    function test() {
+      console.log('test');
+    }
+
+    return { test };
+  },
+
+  //emits: ['toggle-plannerSidebar', 'toggle-chatSidebar'],
 };
 </script>
+
+<style scoped>
+.room-navbar {
+  height: 80px;
+}
+</style>
