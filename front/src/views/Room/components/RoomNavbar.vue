8fb4604461a2bfc41bde248ee7c117b3bdeed8f2
YeoUl <41969902+YeoUlFox@users.noreply.github.com>
2022-07-28 18:01:45 +0900

    FE-feature add video grid, components

--- front/src/views/Room/components/RoomNavbar.vue
+++ front/src/views/Room/components/RoomNavbar.vue
@@ -4,34 +4,36 @@
   >
     <i
       id="planner-sidebar-toggle-icon"
-      class="toggle-btn bi bi-calendar-week align-items-center me-auto p-2 pe-4"
+      class="toggle-btn bi bi-calendar-week align-items-center me-auto p-1 px-3"
       :class="[
-        $root.theme ? 'dark' : 'light',
+        $root.theme ? 'light-btn' : 'dark-btn',
         planner_play_anim ? 'planner-anim-active' : '',
       ]"
       style="
-        font-size: 2rem;
+        font-size: 1em;
         border-top-right-radius: 2rem;
         border-bottom-right-radius: 2rem;
       "
       ref="planner_sidebar_toggle_icon"
       @click="openPlanner()"
     ></i>
-    <button @click="planner_play_anim = true" class="mx-auto">
-      alram Planner
-    </button>
+    
+    <!--
+    <button @click="planner_play_anim = true" class="mx-auto">alram Planner</button>
     <button @click="chat_play_anim = true" class="mx-auto">alram Chat</button>
-    <p id="room-name-text" class="mx-auto">{{ roomname }}</p>
+    -->
+
+    <img src="@/assets/logo/title_logo_009e73.png" class="d-inline-block align-top h-100" alt="logo">
 
     <i
       id="chat-sidebar-toggle-icon"
-      class="toggle-btn bi bi-chat-dots ms-auto p-2 ps-4"
+      class="toggle-btn bi bi-chat-dots ms-auto p-1 px-3"
       :class="[
-        $root.theme ? 'dark' : 'light',
+        $root.theme ? 'light-btn' : 'dark-btn',
         chat_play_anim ? 'chat-anim-active' : '',
       ]"
       style="
-        font-size: 2rem;
+        font-size: 1em;
         border-top-left-radius: 2rem;
         border-bottom-left-radius: 2rem;
       "
@@ -42,35 +44,10 @@
 </template>
 
 <script>
-import { ref, onBeforeMount } from 'vue';
-import { useRouter } from 'vue-router';
+import { ref } from 'vue';
 
 export default {
   setup(props, { emit }) {
-    // get Room name
-    let roomname = ref(null);
-    let router = useRouter();
-    onBeforeMount(() => {
-      let url = document.URL;
-      let idx = url.indexOf('studyroom/') + 10;
-      if (idx === -1) {
-        alert('올바르지 않은 방주소입니다');
-        router.replace('/');
-        return;
-      }
-
-      // check room number is number
-      let room_number = url.slice(idx);
-      if (Number.isInteger(room_number)) {
-        alert('방 번호가 형식에 맞지 않습니다');
-        router.replace('/');
-        return;
-      }
-
-      // TODO : get roomname from server
-      roomname.value = room_number;
-    });
-
     // flags for setting class
     var planner_play_anim = ref(false);
     var chat_play_anim = ref(false);
@@ -86,7 +63,6 @@ export default {
     }
 
     return {
-      roomname,
       openPlanner,
       openChat,
       planner_play_anim,
@@ -111,6 +87,15 @@ export default {
   100% {
     background-color: var(--main-color);
   }
+}
+
+.light-btn {
+  background-color :#545664;
+  color: #EEEEEE;
+}
+.dark-btn {
+  background-color: #EEEEEE;
+  color: #545664;
 }
 
 .planner-anim-active,
