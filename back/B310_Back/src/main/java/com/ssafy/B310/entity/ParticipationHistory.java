89aa0e8968da8fdabb17f4df445f47bd8e3cd3ea
최진호 <ho6778@naver.com>
2022-08-01 17:07:06 +0900

    if join room update roomhistory

--- back/B310_Back/src/main/java/com/ssafy/B310/entity/ParticipationHistory.java
+++ back/B310_Back/src/main/java/com/ssafy/B310/entity/ParticipationHistory.java
@@ -1,11 +1,17 @@
 package com.ssafy.B310.entity;
 
+import java.time.LocalDateTime;
+import java.util.Date;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -30,13 +36,23 @@ public class ParticipationHistory {
 	@JoinColumn(name = "user_num")
 	private User user;
 
-	public ParticipationHistory(int participationHistoryNum, Room room, User user) {
+	@Column
+	private LocalDateTime participationHistoryUpdateTime;
+	
+	public ParticipationHistory() {
+	}
+	public ParticipationHistory(Room room, User user,
+			LocalDateTime participationHistoryUpdateTime) {
+		this.room = room;
+		this.user = user;
+		this.participationHistoryUpdateTime = participationHistoryUpdateTime;
+	}
+	public ParticipationHistory(int participationHistoryNum, Room room, User user,
+			LocalDateTime participationHistoryUpdateTime) {
 		this.participationHistoryNum = participationHistoryNum;
 		this.room = room;
 		this.user = user;
-	}
-
-	public ParticipationHistory() {
+		this.participationHistoryUpdateTime = participationHistoryUpdateTime;
 	}
 	
 	
