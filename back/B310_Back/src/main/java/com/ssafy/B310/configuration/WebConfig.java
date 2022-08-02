98262cf31faf109fad7db8bcf3ee0c6dabddfe80
최진호 <ho6778@naver.com>
2022-08-02 11:41:05 +0900

    solve the JwtInterceptor error

--- back/B310_Back/src/main/java/com/ssafy/B310/configuration/WebConfig.java
+++ back/B310_Back/src/main/java/com/ssafy/B310/configuration/WebConfig.java
@@ -1,17 +1,20 @@
 package com.ssafy.B310.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ssafy.B310.interceptor.JwtInterceptor;
 
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
+	
+	@Autowired
+	JwtInterceptor jwtInterceptor;
 	
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
@@ -22,7 +25,7 @@ public class WebConfig implements WebMvcConfigurer {
 
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(new JwtInterceptor())
+		registry.addInterceptor(jwtInterceptor)
 				.order(1)
 				.addPathPatterns("/**");
 	}
