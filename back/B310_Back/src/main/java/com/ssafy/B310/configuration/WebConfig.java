5fd67bf09b1dd803d6d9fd58576b524176e7220e
seoeun98 <vcv0174@naver.com>
2022-08-04 14:18:06 +0900

    add swagger

--- back/B310_Back/src/main/java/com/ssafy/B310/configuration/WebConfig.java
+++ back/B310_Back/src/main/java/com/ssafy/B310/configuration/WebConfig.java
@@ -8,7 +8,6 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 	
 	@Override
@@ -27,6 +26,10 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		WebMvcConfigurer.super.addResourceHandlers(registry);
+		
+		registry
+        .addResourceHandler("/swagger-ui.html")
+        .addResourceLocations("classpath:/META-INF/resources/");
 		
 		registry.addResourceHandler(uploadPath)
 				.addResourceLocations(resourcePath);
