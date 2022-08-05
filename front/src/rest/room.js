121953cfc36504fd38cf39e067155587c5b6a076
YeoUl <41969902+YeoUlFox@users.noreply.github.com>
2022-08-05 16:07:13 +0900

    FE-feature add rest and fix token

---
+++ front/src/rest/room.js
@@ -0,0 +1,75 @@
+import http from '@/util/common/http-commons';
+import store from '@/store';
+var REST_PATH = '/room';
+
+export default {
+  getRoomList: function (checkVideoOn, checkSoundOn, checkFull) {
+    // video : 0(끔) / 1(켬) / 2(무관)
+    // sound : 0(끔) / 1(켬) / 2(무관)
+    // fullcheck : 0(무관) / 1 (정원이 안찬 방)
+    let params = {
+      video: checkVideoOn,
+      sound: checkSoundOn,
+      fullcheck: checkFull,
+    };
+
+    return new Promise((resolve, reject) => {
+      http
+        .post(REST_PATH + '/filter', params)
+        .then((response) => {
+          resolve(response);
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    });
+  },
+
+  getRoomInfo: function (room_num) {
+    return new Promise((resolve, reject) => {
+      http
+        .get(REST_PATH + '/' + room_num)
+        .then((response) => {
+          resolve(response);
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    });
+  },
+
+  joinRoom: function (room_num, roomPw = 0) {
+    let params = {
+      roomPw: roomPw,
+    };
+
+    return new Promise((resolve, reject) => {
+      http
+        .post(REST_PATH + '/' + room_num, params)
+        .then((response) => {
+          resolve(response);
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    });
+  },
+
+  leaveRoom: function (room_num) {
+    let params = {
+      // TODO : remove store code part later
+      userId: store.getters['getUserID'],
+    };
+
+    return new Promise((resolve, reject) => {
+      http
+        .delete(REST_PATH + '/exit/' + room_num, params)
+        .then((response) => {
+          resolve(response);
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    });
+  },
+};
