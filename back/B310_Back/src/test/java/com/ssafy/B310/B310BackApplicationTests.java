040ab13af58b215abc3eac4d25437b6fbb2adef3
최진호 <ho6778@naver.com>
2022-08-05 17:51:49 +0900

    update week3 develop

---
+++ back/B310_Back/src/test/java/com/ssafy/B310/B310BackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.B310;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B310BackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
