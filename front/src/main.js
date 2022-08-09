430310699141b579bfe89f600b655ce0f472d31b
YeoUl <41969902+YeoUlFox@users.noreply.github.com>
2022-08-09 12:43:50 +0900

    FE-feature add global toast setting

--- front/src/main.js
+++ front/src/main.js
@@ -1,12 +1,18 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
 
 // for bootstrap
-import { BootstrapVue3 } from "bootstrap-vue-3";
+import { BootstrapVue3, BToastPlugin } from 'bootstrap-vue-3';
 // import CSS
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap-vue-3/dist/bootstrap-vue-3.css";
-import "bootstrap-icons/font/bootstrap-icons.css";
-createApp(App).use(router).use(store).use(BootstrapVue3).mount("#app");
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+createApp(App)
+  .use(router)
+  .use(store)
+  .use(BootstrapVue3)
+  .use(BToastPlugin)
+  .mount('#app');
