f111cd1224abdef6b6014d19b1e76b61bc2703fc
seoeun98 <vcv0174@naver.com>
2022-08-10 11:44:02 +0900

    토큰 수정 완

--- back/B310_Back/src/main/java/com/ssafy/B310/jwt/JwtTokenProvider.java
+++ back/B310_Back/src/main/java/com/ssafy/B310/jwt/JwtTokenProvider.java
@@ -3,6 +3,7 @@ package com.ssafy.B310.jwt;
 import java.io.UnsupportedEncodingException;
 import java.util.Base64;
 import java.util.Date;
+import java.util.Optional;
 
 import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletRequest;
@@ -10,9 +11,14 @@ import javax.xml.bind.DatatypeConverter;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import com.ssafy.B310.dto.TokenResponse;
+import com.ssafy.B310.entity.User;
+import com.ssafy.B310.repository.AuthRepository;
+import com.ssafy.B310.repository.UserRepository;
 import com.ssafy.B310.service.JwtServiceImpl;
 
 import io.jsonwebtoken.Claims;
@@ -26,6 +32,13 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Component
 public class JwtTokenProvider {
+	
+	@Autowired
+	AuthRepository authRepository;
+	
+	@Autowired
+	UserRepository userRepository;
+	
 	
 	public static final Logger logger = LoggerFactory.getLogger(JwtServiceImpl.class);
 
@@ -174,5 +187,49 @@ public class JwtTokenProvider {
 		}
 		return key;
 	}
+	
+	
+	public TokenResponse issueAccessToken(HttpServletRequest request){
+        String accessToken = resolveAccessToken(request);
+        String refreshToken = resolveRefreshToken(request);
+        
+        System.out.println("accessToken = " + accessToken);
+        System.out.println("refreshToken = " + refreshToken);
+        
+        //accessToken이 만료됐고 refreshToken이 맞으면 accessToken을 새로 발급(refreshToken의 내용을 통해서)
+        if(isValidRefreshToken(refreshToken)){     //들어온 Refresh 토큰이 유효한지
+            System.out.println("Refresh 토큰은 유효함");
+            
+            Claims claimsToken = getClaimsToken(refreshToken);
+            String userId = (String)claimsToken.get("userId");
+            
+            Optional<User> user = userRepository.findByUserId(userId);
+            System.out.println(user.get());
+            String tokenFromDB = authRepository.findByuser_userId(user.get().getUserId()).get().getRefreshToken();
+            
+            System.out.println("tokenFromDB = " + tokenFromDB);
+            System.out.println(refreshToken);
+            if(refreshToken.equals(tokenFromDB)) {   //DB의 refresh토큰과 지금들어온 토큰이 같은지 확인
+                System.out.println("Access 토큰 재발급 완료");
+                accessToken = createAccessToken(userId);
+                System.out.println("새로운 accessToken ::: " + accessToken);
+            }
+            else{
+                //DB의 Refresh토큰과 들어온 Refresh토큰이 다르면 중간에 변조된 것임
+                System.out.println("Refresh Token Tampered");
+                //예외발생
+                return null;
+            }
+        }
+        else{
+            //입력으로 들어온 Refresh 토큰이 유효하지 않음
+        	return null;
+        }
+        
+        return TokenResponse.builder()
+                .ACCESS_TOKEN(accessToken)
+                .REFRESH_TOKEN(refreshToken)
+                .build();
+    }
     
 }
