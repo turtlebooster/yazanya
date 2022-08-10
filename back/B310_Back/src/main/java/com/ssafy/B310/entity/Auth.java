6d60bfe5585f13a7716969f9715f62384c4cc689
seoeun98 <vcv0174@naver.com>
2022-08-10 17:25:11 +0900

    auth 수정

--- back/B310_Back/src/main/java/com/ssafy/B310/entity/Auth.java
+++ back/B310_Back/src/main/java/com/ssafy/B310/entity/Auth.java
@@ -7,6 +7,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -29,7 +30,7 @@ public class Auth {
     private String refreshToken;
 
     @JsonIgnore
-    @ManyToOne
+    @OneToOne
     @JoinColumn(name = "user_id")
     private User user;
 
