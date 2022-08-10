d1cbe2bfad5e196d3a7e27f3a9854eced307837b
jihoyangKR <97648339+jihoyangKR@users.noreply.github.com>
2022-08-10 13:59:10 +0900

    Room entity cacade 추가

--- back/B310_Back/src/main/java/com/ssafy/B310/entity/Room.java
+++ back/B310_Back/src/main/java/com/ssafy/B310/entity/Room.java
@@ -4,17 +4,7 @@ import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.Set;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
-import javax.persistence.PrePersist;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
+import javax.persistence.*;
 
 import org.hibernate.annotations.ColumnDefault;
 
@@ -97,15 +87,15 @@ public class Room {
 	@Column
 	private int userNum;
 
-	@OneToMany(mappedBy = "room")
+	@OneToMany(mappedBy = "room", cascade = CascadeType.REMOVE)
 	@JsonIgnore
 	private Set<Participation> participationList;
 
-	@OneToMany(mappedBy = "room")
+	@OneToMany(mappedBy = "room", cascade = CascadeType.REMOVE)
 	@JsonIgnore
 	private Set<ParticipationHistory> participationHistoryList;
 
-	@OneToMany(mappedBy = "room")
+	@OneToMany(mappedBy = "room", cascade = CascadeType.REMOVE)
 	@JsonIgnore
 	private Set<RoomHashtag> roomHashtag;
 }
