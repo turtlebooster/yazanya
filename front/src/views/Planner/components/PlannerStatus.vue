5c36ea4e8aca9510ce06b12aca7e607cacf54cd2
JunHyuk Park <73526186+inte9ral@users.noreply.github.com>
2022-08-10 10:35:41 +0900

    FE-feature planner structure little change
    플래너 위젯 컴포넌트들은 최상위에 widget-template, widget-component 순으로 들어가도록 정리 + 입체감을 만드는 class .outer .inner 설정과 공백 선을 만드는 .spacer .liner app.vue 로 뺌

--- front/src/views/Planner/components/PlannerStatus.vue
+++ front/src/views/Planner/components/PlannerStatus.vue
@@ -1,18 +1,18 @@
 <template>
-  <div>
-    <div class="block">
-      <div class="block-icon">
-        <span><i class="bi bi-clipboard2-check-fill"></i></span>
+  <div class="widget-template">
+    <div class="widget-component">
+      <div class="widget-component-icon">
+        <span><i class="bi bi-trophy-fill"></i></span>
       </div>
-      <div class="block-detail">
+      <div class="widget-component-detail">
         <div>목표까지</div>
         <div style="font-size: 16pt">21%</div>
       </div>
     </div>
 
     <!-- modal -->
-    <b-modal id="modal-4" centered title="공부티어">
-      <p class="my-4">대충 티어</p>
+    <b-modal id="modal-3" centered title="프로필">
+      <p class="my-4">대충 배너</p>
     </b-modal>
   </div>
 </template>
@@ -23,22 +23,19 @@ export default {};
 
 <style scoped>
 .widget-component {
-  /* background: linear-gradient(-30deg, #b576f6, #9a4fe9); */
-  /* background: linear-gradient(-30deg, #6aa5e3, #6866e9); */
+  width: 100%;
+  height: 100%;
+  padding: 16px;
   background: linear-gradient(-30deg, #feb683, #ff8993);
 }
 
-.block {
-  padding: 16px;
-}
-
-.block * {
+.widget-component * {
   font-size: 20pt;
   color: white;
 }
 
-.block::before,
-.block::after {
+.widget-component::before,
+.widget-component::after {
   content: '';
   position: absolute;
   top: -5%;
@@ -49,13 +46,13 @@ export default {};
   border-radius: 100%;
 }
 
-.block::after {
+.widget-component::after {
   top: auto;
   bottom: -5%;
   right: -5%;
 }
 
-.block-icon {
+.widget-component-icon {
   margin-bottom: 15px;
   height: 60px;
   width: 60px;
