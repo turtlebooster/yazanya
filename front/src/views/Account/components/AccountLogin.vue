cadfbb049c76fdaccaaa8b080c5bad3292f12c9f
YeoUl <41969902+YeoUlFox@users.noreply.github.com>
2022-08-14 23:06:19 +0900

    FE-feature fix error
    방 나가기 중첩 에러
    채팅 프로필 링크 수정

--- front/src/views/Account/components/AccountLogin.vue
+++ front/src/views/Account/components/AccountLogin.vue
@@ -97,8 +97,7 @@ export default {
       try {
         let tokens = await rest_user.login({ id: id.value, pw: pw.value })
         await store.dispatch('login', {'access-token': tokens.access_TOKEN, 'refresh-token': tokens.refresh_TOKEN, id: id.value });
-
-        console.log(store.state.Account.nextRoom);
+        
         if(store.state.Account.nextRoom != '') {
           // move to room
           router.replace(store.state.Account.nextRoom);
