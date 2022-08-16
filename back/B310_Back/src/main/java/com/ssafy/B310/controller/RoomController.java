a5ad220d44e66f7828a543436a283e0fdd988d46
jihoyangKR <97648339+jihoyangKR@users.noreply.github.com>
2022-08-17 00:24:39 +0900

    swagger 일부수정

--- back/B310_Back/src/main/java/com/ssafy/B310/controller/RoomController.java
+++ back/B310_Back/src/main/java/com/ssafy/B310/controller/RoomController.java
@@ -319,9 +319,9 @@ public class RoomController {
 
 	@GetMapping("/search")
 	@ApiOperation(value = "추천 방 리스트",
-			notes = "해쉬태그 번호들을 받아 관련된 방 목록 전달\r\n"
-					+ "hashtagNum=1,2,3,4,5")
-	@ApiImplicitParam(name = "hashtagNum", value = "해쉬태그 번호")
+			notes = "해쉬태그 이름들을 받아 관련된 방 목록 전달\r\n"
+					+ "hashtagName=공시, 임용고시, 공부")
+	@ApiImplicitParam(name = "hashtagName", value = "해쉬태그 이름")
 	public ResponseEntity<?> searchRoom(@RequestParam(value="hashtagName", required=false, defaultValue="") List<String> hashtagNameList) {
 		return new ResponseEntity<Map<String, Object>>(roomservice.searchRecommendHashtagList(hashtagNameList), HttpStatus.OK);
 	}
