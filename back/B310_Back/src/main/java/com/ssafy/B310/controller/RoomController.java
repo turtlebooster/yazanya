6d83960c4f0ff07e668f99fb7b76d6a3522e2d83
YeoUl <41969902+YeoUlFox@users.noreply.github.com>
2022-08-17 01:05:40 +0900

    BE-feature fix naming error

--- back/B310_Back/src/main/java/com/ssafy/B310/controller/RoomController.java
+++ back/B310_Back/src/main/java/com/ssafy/B310/controller/RoomController.java
@@ -313,7 +313,7 @@ public class RoomController {
     			  notes = "해쉬태그 번호들을 받아 관련된 방 목록 전달\r\n"
     				+ "hashtagNum=1,2,3,4,5")
     @ApiImplicitParam(name = "hashtagNum", value = "해쉬태그 번호")
-	public ResponseEntity<?> searchRoom(@RequestParam(value="hashtagName", required=false, defaultValue="") List<String> hashtagNameList) {
+	public ResponseEntity<?> recommendRoom(@RequestParam(value="hashtagName", required=false, defaultValue="") List<String> hashtagNameList) {
 		return new ResponseEntity<Map<String, Object>>(roomservice.getRecommendHashtagList(hashtagNameList), HttpStatus.OK);
 	}
 
