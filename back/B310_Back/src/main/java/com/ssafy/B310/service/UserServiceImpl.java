1ab81726e3b34ef1267045480220677384ca1472
최진호 <ho6778@naver.com>
2022-08-17 01:32:02 +0900

    BE-feature User update empty check

--- back/B310_Back/src/main/java/com/ssafy/B310/service/UserServiceImpl.java
+++ back/B310_Back/src/main/java/com/ssafy/B310/service/UserServiceImpl.java
@@ -89,8 +89,8 @@ public class UserServiceImpl implements UserService{
 		
 		if (oUser.isPresent()) {
 			User u = oUser.get();
-			if (user.getUserPw() != null) u.setUserPw(hashPw(user.getUserPw()));
-			if (user.getUserNickname() != null) u.setUserNickname(user.getUserNickname());
+			if (user.getUserPw() != null || user.getUserPw().trim().equals("")) u.setUserPw(hashPw(user.getUserPw()));
+			if (user.getUserNickname() != null || user.getUserNickname().trim().equals("")) u.setUserNickname(user.getUserNickname());
 			if (user.getProfileSelfIntroduce() != null) u.setProfileSelfIntroduce(user.getProfileSelfIntroduce());
 			
 			userRepository.save(u);
